6fbe5624086c54e3cb88777fe8ced6f50a345cf6
quyi-ai <djjdkdj829@gmail.com>
2026-05-13 17:04:39 +0800

    .

--- Resnet18/train.py
+++ Resnet18/train.py
@@ -2,6 +2,10 @@ from pathlib import Path
 
 import torch
 from torchvision import datasets, transforms
+import torch.nn as nn
+from torchvision import models
+from torchvision.models import ResNet18_Weights
+
 DATA_DIR = Path(__file__).resolve().parent / "data" / "hymenoptera_data"
 BATCH_SIZE = 4
 data_transforms = {
@@ -46,8 +50,41 @@ class_names = image_datasets["train"].classes
 
 print("train classes:", class_names)
 
-images, labels = next(iter(dataloaders["train"]))
 
-print("images shape:", images.shape)
-print("labels shape:", labels.shape)
-print("labels:", labels)
+num_classes = 2
+weights=ResNet18_Weights.DEFAULT
+model = models.resnet18(weights=weights)
+criterion=nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(
+    model.fc.parameters(),
+    lr=0.001,
+    momentum=0.9
+)
+num_epochs = 5
+
+for epoch in range(num_epochs):
+    model.train()
+
+    running_loss = 0.0
+    running_corrects = 0
+
+    for images, labels in dataloaders["train"]:
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        _, preds = torch.max(outputs, 1)
+
+        running_loss += loss.item() * images.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+    epoch_loss = running_loss / len(image_datasets["train"])
+    epoch_acc = running_corrects.double() / len(image_datasets["train"])
+
+    print(f"Epoch {epoch + 1}/{num_epochs}")
+    print(f"train loss: {epoch_loss:.4f}")
+    print(f"train acc: {epoch_acc.item():.4f}")
+    print("-" * 30)  
